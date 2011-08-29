e3d6604859af89e8cc941a4734f26a8879d3ecda
Kelley van Evert <kelley.vanevert@gmail.com>
2011-08-30 00:45:27 +0200

    Function.prototype.bind() fallback added, now it works in Firefox ;)

--- blokus.js
+++ blokus.js
@@ -20,6 +20,21 @@ var Blokus = (function() {
             });
         };
     }
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function(oThis) {
+            var aArgs = Array.prototype.slice.call(arguments, 1), 
+                fToBind = this, 
+                fNOP = function() {},
+                fBound = function() {
+                    return fToBind.apply(this instanceof fNOP ? this : oThis || window, aArgs.concat(Array.prototype.slice.call(arguments)));    
+                };
+            
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+            
+            return fBound;
+        };
+    }
     
     // Simple vector class
     var Vec = function(x, y) {
